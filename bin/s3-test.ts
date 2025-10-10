bf0adaf86fcfe05ac5fe6c0fdb611be4ce894fb5
mquang9x <101198738+mquang9x@users.noreply.github.com>
2025-10-10 21:10:15 +0700

    delete s3 files

--- bin/s3-test.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { S3BucketStack } from '../lib/s3-bucket-stack';
-
-const app = new cdk.App();
-new S3BucketStack(app, 'S3BucketStack');
